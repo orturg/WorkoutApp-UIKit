5a5ad3fac067d761840dfc07c523d131fb9061ea
Artur <nozhenkoartur2108@gmail.com>
2024-04-28 13:21:35 +0300

    Initial Commit

---
+++ WorkoutApp UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp UIKit
+//
+//  Created by Artur Nozhenko on 28.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
